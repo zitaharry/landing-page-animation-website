c561d3baa191185557bfc9382ce78b616b29caea
zitaharry <harrymuigua@gmail.com>
2025-06-24 21:43:23 +0300

    Implement Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +14,12 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants/index.js";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
